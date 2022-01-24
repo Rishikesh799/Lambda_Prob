51c515589da8f0fbc138dd481c8bf2a66f009c66
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-01-24 22:17:26 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,4 +1,4 @@
-"""
+"""-----------------------------LAMBDA PROBLEM---------------------------------------------------------
 #1. Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument, also create a lambda function that multiplies argument x with argument y and print the result
 
 x= lambda i:(i+15)
